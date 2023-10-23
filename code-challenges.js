98279fd9b0e065660b1af17b4a87485e13567d22
chandlergipson <chandlergipson96@gmail.com>
2023-10-22 18:40:16 -0700

    assessment complete

--- code-challenges.js
+++ code-challenges.js
@@ -26,7 +26,16 @@ const fruit2 = "banana"
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
-console.log(padres1984WorldSeriesRuns.length + padres1998WorldSeriesRuns.length)
+const testArray = [3, 4, 45, 65, 2, 999]
+const arrayTest = [7, 8]
+const lastArray = [1, 2, 3,]
+// console.log(padres1984WorldSeriesRuns.length + padres1998WorldSeriesRuns.length)
+
+const worldSeriesRunsCalculator = (base, array2, array3, array4) => {
+    return base.length + array2.length + array3.length + array4.length
+}
+console.log(worldSeriesRunsCalculator(arrayTest, testArray, padres1998WorldSeriesRuns, padres1984WorldSeriesRuns))
+
 // Expected output: 9
 
 
@@ -39,7 +48,7 @@ console.log(padres1984WorldSeriesRuns.length + padres1998WorldSeriesRuns.length)
 const currentCohort = "Hotel 2023"
 // Expected output: "3202 floG"
 // currentCohort.split('').reverse().join('')
-console.log(currentCohort.split('').reverse().join(''))
+// console.log(currentCohort.split('').reverse().join(''))
 // // Pseudo code:
 //input: i  wanted to flip the const but in order to turn a string into an array i need to .split it first
 //output: once the string has been spit i can .reverse it, following the .reverse i need to .join it. i added all of that into the conse.log
@@ -50,11 +59,11 @@ console.log(currentCohort.split('').reverse().join(''))
 
 const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
 // Expected output: 13 5 -5 27
-for(let i = 0 ; i < stockExchange.length; i++){
-    if(stockExchange[i] % 2 !== 0){
-    console.log(stockExchange[i])
-    }
-}
+// for(let i = 0 ; i < stockExchange.length; i++){
+//     if(stockExchange[i] % 2 !== 0){
+//     console.log(stockExchange[i])
+// //     }
+// }
 
 // Pseudo code:
 //when it comes to evens or odds i know you need to use %. % means cannot be equal to.  im let index = 0 my index connot be larger than the array
